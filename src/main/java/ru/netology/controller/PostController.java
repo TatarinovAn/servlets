a16a55ef3d3a13b0ea8788a9cfafb33cbadc9ff2
TatarinovAn <gitaline@bk.ru>
2024-02-06 00:14:19 +0300

    change postController

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -1,61 +1,37 @@
 package ru.netology.controller;
 
-import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.*;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.Reader;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
-    public static final String APPLICATION_JSON = "application/json";
     private final PostService service;
-    private final Gson gson = new Gson();
 
     public PostController(PostService service) {
         this.service = service;
     }
 
     @GetMapping
-    public void all(HttpServletResponse response) {
-        senderResponse(response, service.all());
+    public List<Post> all() {
+        return service.all();
     }
 
     @GetMapping("/{id}")
-    public void getById(@PathVariable long id, HttpServletResponse response) {
-        senderResponse(response, service.getById(id));
+    public Post getById(@PathVariable long id) {
+        return service.getById(id);
     }
 
     @PostMapping
-    public void save(Reader body, HttpServletResponse response) {
-
-        final var data = gson.fromJson(body, Post.class);
-        service.save(data);
-        senderResponse(response, data);
-
+    public Post save(@RequestBody Post post) {
+        return service.save(post);
     }
 
     @DeleteMapping("/{id}")
-    public void removeById(@PathVariable long id, HttpServletResponse response) {
-
+    public void removeById(@PathVariable long id) {
         service.removeById(id);
-        try {
-            response.getWriter().print("Delete ok");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private <T> void senderResponse(HttpServletResponse response, T data) {
-        response.setContentType(APPLICATION_JSON);
-        try {
-            response.getWriter().print(gson.toJson(data));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
